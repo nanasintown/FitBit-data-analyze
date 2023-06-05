010a64e0cafd261691bb13465d80fea1f6334b62
nanacao <nhutmcao@gmail.com>
2023-06-05 21:38:04 +0300

    Get activity summary

--- Watch_Request.py
+++ Watch_Request.py
@@ -11,7 +11,7 @@ REFRESH_TOKEN = 9d90392d0b53a9c3db6d975858cb01829c3e7d92cdb6cf3382fa49895c256446
 # Watch_Request object handles authentication and http requests for biometrical data.
 # get-functions try to handle the cases when no data is not available (E.g. watch was not worn) by filling nodata with NaN's
 # With the API, you can do up to 150 http requests per user / hour. If you doo too many you are going to get a HTTPTooManyRequests -error.
-ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1FaVEIiLCJzdWIiOiJCSkZRWEQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd2VjZyB3c29jIHdhY3Qgd294eSB3dGVtIHd3ZWkgd2NmIHdzZXQgd2xvYyB3cmVzIiwiZXhwIjoxNjg1Mjk1NDYwLCJpYXQiOjE2ODUyNjY2NjB9.JJP2dKCT2u3tnsUiT0YMdAEYfiOu-bUZO8ZulQvKh-U"
+ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1FaVEIiLCJzdWIiOiJCSkZRWEQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd2VjZyB3c29jIHdhY3Qgd294eSB3dGVtIHd3ZWkgd2NmIHdzZXQgd3JlcyB3bG9jIiwiZXhwIjoxNjg2MDE1OTc0LCJpYXQiOjE2ODU5ODcxNzR9.gK_1RafFuNyhQqstnns1cHlvYuhJW5AuLV6d4BDUt58"
 
 class Watch_Request(object):
     def __init__(self, CLIENT_ID, CLIENT_SECRET):
@@ -69,6 +69,23 @@ class Watch_Request(object):
         final_df = pd.concat(df_list, axis = 0)
         return final_df
 
+    def get_activities_log(self, startdate, enddate):
+        df_dict = {}
+        header = {"Authorization": "Bearer {}".format(ACCESS_TOKEN)}
+        options = ["calories", "steps", "distance", "floors", "minutesSedentary", "minutesLightlyActive", "minutesFairlyActive", "minutesVeryActive", "activityCalories"]
+        all_dates = pd.date_range(start=startdate, end=enddate)
+        dates = []
+        for day in all_dates:
+            day = day.date().strftime("%Y-%m-%d")
+            dates.append(day)
+        for option in options:
+            response = requests.get(f"https://api.fitbit.com/1/user/-/activities/tracker/{option}/date/{startdate}/{enddate}.json", headers=header).json()
+            data = pd.DataFrame(response[f"activities-tracker-{option}"])
+            df_dict[option] = list(data["value"])
+        df = pd.DataFrame(df_dict)
+        df.insert(0, "Date", dates, True)
+        return df
+
 def save_as_csv(filename, data):
     path = "fitbit-data/"
     data.to_csv(path + filename, index=False)
@@ -86,3 +103,7 @@ if __name__ == "__main__":
 
     # calories_data = wr.get_calories("04-17-2023", "05-27-2023")
     # save_as_csv("calories_data.csv", calories_data)
+
+    # activity summary
+    activity_summary = wr.get_activities_log("2023-05-01", "2023-05-31")
+    save_as_csv("activity_may_summary.csv", activity_summary)
